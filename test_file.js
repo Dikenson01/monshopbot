c7febaacd0b8ba45be7cfb4b7bdb90b30c59ac88
dikenson01 <mailletmatthieu@icloud.com>
2026-05-23 21:19:06 +0200

    chore: remove backup files

--- test_file.js
+++
@@ -1,3 +0,0 @@
-function t() {}
-const ctx = { reply: () => {} };
-ctx.reply(t(ctx, 'msg_vous_n_tes_pas_enco', "❌ Vous n\'êtes pas encore abonné au canal. Veuillez cliquer sur \"Rejoindre le Canal\" puis réessayer."), { parse_mode: 'HTML' });
